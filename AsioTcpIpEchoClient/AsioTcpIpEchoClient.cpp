1c658ad5d5ab5842793ac3e9f870ead0145df2ab
SEIN <lovesick9909@gmail.com>
2024-10-29 08:21:48 +0900

    기본 뼈대 구축
    asio 공식 문서의 echo 서버/클라이언트를 vs 환경에 맞게 수정하고 빌드가 되는 것 까지 확인

---
+++ AsioTcpIpEchoClient/AsioTcpIpEchoClient.cpp
@@ -0,0 +1,86 @@
+﻿
+#include "Client.h"
+
+//
+// This class manages socket timeouts by applying the concept of a deadline.
+// Some asynchronous operations are given deadlines by which they must complete.
+// Deadlines are enforced by an "actor" that persists for the lifetime of the
+// client object:
+//
+//  +----------------+
+//  |                |
+//  | check_deadline |<---+
+//  |                |    |
+//  +----------------+    | async_wait()
+//              |         |
+//              +---------+
+//
+// If the deadline actor determines that the deadline has expired, the socket
+// is closed and any outstanding operations are consequently cancelled.
+//
+// Connection establishment involves trying each endpoint in turn until a
+// connection is successful, or the available endpoints are exhausted. If the
+// deadline actor closes the socket, the connect actor is woken up and moves to
+// the next endpoint.
+//
+//  +---------------+
+//  |               |
+//  | start_connect |<---+
+//  |               |    |
+//  +---------------+    |
+//           |           |
+//  async_-  |    +----------------+
+// connect() |    |                |
+//           +--->| handle_connect |
+//                |                |
+//                +----------------+
+//                          :
+// Once a connection is     :
+// made, the connect        :
+// actor forks in two -     :
+//                          :
+// an actor for reading     :       and an actor for
+// inbound messages:        :       sending heartbeats:
+//                          :
+//  +------------+          :          +-------------+
+//  |            |<- - - - -+- - - - ->|             |
+//  | start_read |                     | start_write |<---+
+//  |            |<---+                |             |    |
+//  +------------+    |                +-------------+    | async_wait()
+//          |         |                        |          |
+//  async_- |    +-------------+       async_- |    +--------------+
+//   read_- |    |             |       write() |    |              |
+//  until() +--->| handle_read |               +--->| handle_write |
+//               |             |                    |              |
+//               +-------------+                    +--------------+
+//
+// The input actor reads messages from the socket, where messages are delimited
+// by the newline character. The deadline for a complete message is 30 seconds.
+//
+// The heartbeat actor sends a heartbeat (a message that consists of a single
+// newline character) every 10 seconds. In this example, no deadline is applied
+// to message sending.
+//
+
+
+int main()
+{
+    boost::asio::io_context io_context;
+    tcp::resolver r(io_context);
+    client c(io_context);
+    tcp::resolver::query q("127.0.0.1", "3333");
+
+    try
+    {
+        //c.start(r.resolve(argv[1], argv[2])); -- "Usage: client <host> <port>\n";
+        c.start(r.resolve(q));
+
+        io_context.run();
+    }
+    catch (std::exception& e)
+    {
+        std::cerr << "Exception: " << e.what() << "\n";
+    }
+
+    return 0;
+}
---
+++ AsioTcpIpEchoServer/AsioTcpIpEchoServer.cpp
@@ -0,0 +1,35 @@
+﻿
+#include "Server.h"
+
+/*  < Echo 서버 시나리오 >
+* 0. 서버 시작
+* 1. 접속대기 소켓 생성
+* 2. 포트 바인딩
+* 3. 접속대기 상태로 전환
+* 4. 클라이언트 접속 처리 및 대응
+* 4-1. 클라이언트 연결을 받아들이고 새로운 소켓 생성(개방)
+* 4-2. 클라이언트로부터 수신
+* 4-3. 수신한 거 그대로 다시 전송
+* 4-4. 클라이언트의 연결 종료
+* 5. 리슨 소켓 닫기
+* 6. 서버 정상 종료
+*/
+
+int main()
+{
+    boost::asio::io_context io_context;
+
+    // server s(io_context, std::atoi(argv[1])); 
+    server s(io_context, 3333/*port*/);
+
+    try
+    {
+        io_context.run();
+    }
+    catch (std::exception& e)
+    {
+        std::cerr << "Exception: " << e.what() << "\n";
+    }
+
+    return 0;
+}
